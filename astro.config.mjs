6acaa35b61a191b57356552b6555c3540e410817
therexone <ayushsingh871@gmail.com>
2025-06-09 17:09:39 +0530

    feat: add Netlify adapter for server render support

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,13 @@ import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
+import netlify from '@astrojs/netlify'
 import { remarkReadingTime } from './src/utils/readTime.ts'
 
 // https://astro.build/config
 export default defineConfig({
+	adapter: netlify(),
+	
 	site: 'https://blog.refix.ai', // Replace with your actual domain
 	base: '/blog',
 	trailingSlash: 'always',
